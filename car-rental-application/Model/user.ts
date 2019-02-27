15c93895f400f910e1989e76ec552bd29d2d6733
PratikP93 <patil.prat@husky.neu.edu>
2019-02-27 01:12:43 -0500

    Refactoring branch

--- car-rental-application/Model/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-  username: string;
-  password: string;
-  constructor(username: string, pwd: string) {
-      this.username = username;
-      this.password = pwd;
-  }
-}
--- car-rental-application/src/app/place.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlacePipe } from './place.pipe';
-
-describe('PlacePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlacePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
